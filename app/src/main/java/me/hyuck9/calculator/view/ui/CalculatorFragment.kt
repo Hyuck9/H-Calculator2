c283bcf2f5f259bb7086e9b07098d4d82573210b
Hyuck <lhg1304@nexmore.co.kr>
2022-06-26 18:11:23 +0900

    글자 크기 변경될 때 애니메이션 적용

--- app/src/main/java/me/hyuck9/calculator/view/ui/CalculatorFragment.kt
+++ app/src/main/java/me/hyuck9/calculator/view/ui/CalculatorFragment.kt
@@ -1,14 +1,19 @@
 package me.hyuck9.calculator.view.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator.ofFloat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import me.hyuck9.calculator.R
 import me.hyuck9.calculator.databinding.FragmentCalculatorBinding
 import me.hyuck9.calculator.extensions.observeLiveData
 import me.hyuck9.calculator.view.base.BaseFragment
+import me.hyuck9.calculator.view.custom.CalculatorEditText
 import me.hyuck9.calculator.view.viewmodel.CalculatorInputViewModel
 
 class CalculatorFragment : BaseFragment() {
@@ -27,8 +32,13 @@ class CalculatorFragment : BaseFragment() {
 			container
 		).apply {
 			viewModel = calcViewModel
+			bindViews()
 			addObservers()
 		}.root
+	}
+
+	private fun FragmentCalculatorBinding.bindViews() {
+		inputField.input.setOnTextSizeChangeListener(formulaOnTextSizeChangeListener)
 	}
 
 	private fun addObservers() {
@@ -36,4 +46,32 @@ class CalculatorFragment : BaseFragment() {
 			calcViewModel.calculateOutput()
 		}
 	}
+
+
+	private val formulaOnTextSizeChangeListener =
+		object : CalculatorEditText.OnTextSizeChangeListener {
+			override fun onTextSizeChanged(textView: TextView, oldSize: Float) {
+				val textScale = oldSize / textView.textSize
+				val translationX = (1 - textScale) * (textView.width / 2 - textView.paddingEnd)
+				val translationY = (1 - textScale) * (textView.height / 2 - textView.paddingBottom)
+				animatorSetStart(textView, textScale, translationX, translationY)
+			}
+		}
+
+	private fun animatorSetStart(
+		textView: TextView,
+		textScale: Float,
+		translationX: Float,
+		translationY: Float
+	) = AnimatorSet().apply {
+		playTogether(
+			ofFloat(textView, "scaleX", textScale, 1.0f),
+			ofFloat(textView, "scaleY", textScale, 1.0f),
+			ofFloat(textView, "translationX", translationX, 1.0f),
+			ofFloat(textView, "translationY", translationY, 1.0f)
+		)
+		duration = resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+		interpolator = AccelerateDecelerateInterpolator()
+	}.start()
+
 }
